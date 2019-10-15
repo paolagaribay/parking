cf7b04dc95e6a47ae324d703579749ed29772ded
paolagaribay <54656030+paolagaribay@users.noreply.github.com>
2019-10-15 02:56:03 -0500

    Set up main.java

--- src/main.java
+++ src/main.java
@@ -1,1 +1,11 @@
+import java.io.*; 
 
+public class main {
+  public static void main(String[] args) {
+        // read file
+        // create parking lot w/ capacity & price
+        // create car for each entering
+        // park car
+        // remove car for each leaving
+    }
+}
