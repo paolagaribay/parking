16de9146e99498bd655e95a848a4cbdc9be5b4e7
paolagaribay <54656030+paolagaribay@users.noreply.github.com>
2019-10-15 03:10:49 -0500

    Set up parkingLot.java

--- src/parkingLot.java
+++ src/parkingLot.java
@@ -1,1 +1,24 @@
-//class parkingLot
+
+public class parkingLot {
+  // capacity
+  // price
+  // list of cars parked
+  // counter
+  parkingLot(int c, int p) {
+    // set capacity
+    // set price
+    // create list
+  }
+  public boolean checkCapacity() {
+    // check if parking lot full 
+  }
+  public void entering(Car c, time t) {
+    // if not full - set in time
+    // add car to list
+  }
+  public void leaving(Car c, time t) {
+    // if car in list - set out time
+    // pay ticket
+    // remove car from list
+  }
+}
