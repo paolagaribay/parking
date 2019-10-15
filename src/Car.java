7db8cedeb7737119cac4f71bff1359658736971e
paolagaribay <54656030+paolagaribay@users.noreply.github.com>
2019-10-15 04:05:09 -0500

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -8,4 +8,9 @@ public class Car {
   public String getCarId() {
     // return carid
   }
+  public void set Ticket(String t //or Ticket t) {
+    // set ticket
+  }
+  public String getTicket() {
+    // return ticket
 }
