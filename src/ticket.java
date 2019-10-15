7d33db2d4884300339d5cd92c1ba17ba946a7b04
paolagaribay <54656030+paolagaribay@users.noreply.github.com>
2019-10-15 03:16:24 -0500

    Set up ticket.java

--- src/ticket.java
+++ src/ticket.java
@@ -1,1 +1,16 @@
 
+public class Ticket {
+  // in time
+  // out time
+  public void setInTime(time t) {
+    // set time
+  }
+  public void setOutTime(time t) {
+    // set time
+  }
+  public time getInTime() {
+    // return time
+  }
+  public time getOutTime() {
+    // return time
+  }
